7c1dc3d00c38d6b4283500bf249c20222bbfa8c7
cassidylinhares <cassidy.linhares@ontariotechu.net>
2021-03-26 13:05:05 -0400

    cleaned

--- test_model.py
+++ test_model.py
@@ -1,38 +1,38 @@
 import numpy as np
 import math
+from training_model import spam_indexes, spam_probability, nonspam_probability
 
 # read test-features & test-labels and make into Nx3 array
-arr = []
+features = []
 labels = []
-numFeatures = 0
-with open('test-features.txt') as feat:
-    for i,line in enumerate(feat):
+with open('test-features.txt') as feature_file:
+    for i,line in enumerate(feature_file):
         ar = line.split()
         arr.append(ar)
         numFeatures = i+1
 
-with open('test-labels.txt') as l:
-    for i,line in enumerate(l):
+with open('test-labels.txt') as label_file:
+    for i,line in enumerate(label_file):
         labels += line.split()
 
 # make a np matrix
-matrix = np.array(arr, dtype=int)
-label = np.array(labels, dtype=int)
+features = np.array(features, dtype=int)
+labels = np.array(labels, dtype=int)
 
 # get number of emails
-numTestEmails = len(np.unique(matrix[:, 0]))
+num_test_emails = len(np.unique(matrix[:, 0]))
 
-output = np.zeros(numTestEmails)
+output = np.zeros(num_test_emails)
 
 #probability that one email is spam = # of spam emails/# of all emails
-chanceOfSpam = 1/numTestEmails #len(spam_indicies)/numTestEmails
+prob_spam = len(spam_indexes)/num_test_emails
 
 # get chance for spam and non-spam emails in the test set
-log_spam = matrix * np.log(probFeatSpam.getH()) + math.log(chanceOfSpam)
-log_ham = matrix * np.log(probFeatHam.getH()) + math.log(1-chanceOfSpam)
+log_spam = features * np.log(spam_probability.getH()) + math.log(prob_spam)
+log_ham = features * np.log(nonspam_probability.getH()) + math.log(1-prob_spam)
 
 output = log_spam > log_ham
 
 # get the number of incorrectly predicted
-wrongLabelled = np.sum(output != label)
-err = wrongLabelled/numTestEmails
+wrong_labelled = np.sum(output != labels)
+err = wrong_labelled/num_test_emails
